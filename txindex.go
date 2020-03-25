1c4586c17d6b1a4f261c6f3c03162a5b4bcb4be6
0xb10c <0xb10c@gmail.com>
2020-03-25 23:50:20 +0100

    add: transaction index parsing

---
+++ txindex.go
@@ -0,0 +1,78 @@
+package bitcoindat
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+// TransactionIndexReader reads TransactionIndex's from a LevelDB
+type TransactionIndexReader struct{
+	db *leveldb.DB
+}
+
+// TransactionIndex holds the block position of a transaction
+type TransactionIndex struct{
+	blockFile int
+    blockPos int
+    txOffset int
+}
+
+// OpenTransactionIndexReader opens a transaction index level db.
+// The DB must be closed after use, by calling Close method.
+func (p *BitcoinDAT) OpenTransactionIndexReader() (*TransactionIndexReader, error) {
+	db, err := p.openDB(p.datPath + "/indexes/txindex")
+	if err != nil {
+		return nil, err
+	}
+
+	return &TransactionIndexReader{db}, nil
+}
+
+// Close closes the underlying LevelDB
+func (tir *TransactionIndexReader) Close (){
+	tir.db.Close()
+}
+
+// ReadTXID reads a TransactionIndex from the LevelDB. This function wraps Read()
+func (tir *TransactionIndexReader) ReadTXID(txid string) (*TransactionIndex, error){
+	if len(txid) != 64 {
+		return nil, fmt.Errorf("Expected txid string to have a length of 64 chars; got %d for txid: %s", len(txid), txid)
+	}
+
+	b, err := hex.DecodeString(txid)
+	if err != nil {
+		return nil, err
+	}
+
+	return tir.Read(b)
+}
+
+// Read reads a TransactionIndex from the LevelDB
+func (tir *TransactionIndexReader) Read(txid []byte) (*TransactionIndex, error){
+	if len(txid) != 32 {
+		return nil, fmt.Errorf("Expected txid byte array to have a length of 32 byte; got %d for txid: %s", len(txid), hex.EncodeToString(txid))
+	}
+	
+	// reverse txid
+	for i := len(txid)/2 - 1; i >= 0; i-- {
+		opp := len(txid) - 1 - i
+		txid[i], txid[opp] = txid[opp], txid[i]
+	}
+
+	key := append([]byte("t"), txid...)
+	value, err := tir.db.Get(key,nil)
+	if err!=nil{
+		return nil, err
+	}
+
+	mb := NewMovingBytes(value)
+	ti := &TransactionIndex{}
+	ti.blockFile = int(mb.ReadVarInt())
+	ti.blockPos = int(mb.ReadVarInt())
+	ti.txOffset = int(mb.ReadVarInt())
+
+	return ti, nil
+}
+
---
+++ txindex_test.go
@@ -0,0 +1,21 @@
+package bitcoindat
+
+import "testing"
+
+func TestRead(t *testing.T) {
+	b := NewBitcoinDAT(regtest0)
+
+	tir, err := b.OpenTransactionIndexReader()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ti, err := tir.ReadTXID("624d44c8b38b03fb789d9c50282bf67c5afc6959d08fc39c4f789047754a985f")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ti.blockFile != 0 || ti.blockPos != 45415 || ti.txOffset != 1 {
+		t.Errorf("Expected %+v, but got %+v", &TransactionIndex{0, 45415, 1}, ti)
+	}
+}
